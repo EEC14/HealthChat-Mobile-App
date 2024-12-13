c0e117b649bcc8c5d7b6d17a88a302a99e1f9c02
shebll <shebl4900@gmail.com>
2024-12-13 07:28:27 +0200

    update the env file

--- firebase.ts
+++ firebase.ts
@@ -22,13 +22,13 @@ import {
 import { Message, UserProfile } from "./types";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDseSpx5tJ1mpF0S0vFV2dV-S9kWtI2jb8",
-  authDomain: "patientchat-3d4ee.firebaseapp.com",
-  projectId: "patientchat-3d4ee",
-  storageBucket: "patientchat-3d4ee.firebasestorage.app",
-  messagingSenderId: "703134724815",
-  appId: "1:703134724815:web:217efa7b689b8c1a807dba",
-  measurementId: "G-FRPKX9388C",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
